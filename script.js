f2797070974c099a56522cf1e8122f1b5607a17c
IgorVn <112585504+Igor9090@users.noreply.github.com>
2024-08-20 12:08:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Cristiano Ronaldo Jr é um jovem de 15 anos que encontrou uma paixão no futebol e estuda linguagens de programação, ele se encontra indeciso entre as seguintes opcções:",
+        alternativas: [
+            "Focar nos seus treinos de Futebol e deixar os estudo programação de lado",
+            "Focar nos estudos de programação e deixar os treinos de futebol de lado"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]; 
+
